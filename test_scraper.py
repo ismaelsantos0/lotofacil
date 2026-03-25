2c8914d595325edb8ac390fbbee8297d1cad6bcf
Ismael Santos <ismaelifrr@gmail.com>
2026-03-24 21:31:52 -0400

    Delete test_scraper.py

--- test_scraper.py
+++
@@ -1,8 +0,0 @@
-from scraper_caixa import fetch_latest_results
-
-concursos = fetch_latest_results(limit=5, headless=False)
-
-for c in concursos:
-    print(f"Concurso {c.numero} - {c.data}")
-    print(" ".join(f"{n:02d}" for n in c.dezenas))
-    print("-" * 40)
